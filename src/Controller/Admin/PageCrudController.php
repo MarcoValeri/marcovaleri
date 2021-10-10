fa0952ebf243a65b6f353730c5abcc7e8b66b8c7
Marco Valeri <marcovaleri@hotmail.it>
2021-10-10 21:56:23 +0100

    Create the style for the pages

--- src/Controller/Admin/PageCrudController.php
+++ src/Controller/Admin/PageCrudController.php
@@ -5,6 +5,8 @@ namespace App\Controller\Admin;
 use App\Entity\Page;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 
 class PageCrudController extends AbstractCrudController
 {
@@ -19,6 +21,12 @@ class PageCrudController extends AbstractCrudController
             //IdField::new('id'),
             //TextField::new('title'),
             //TextEditorField::new('description'),
+            TextField::new('title'),
+            TextField::new('description'),
+            TextField::new('url'),
+            DateTimeField::new('date'),
+            DateTimeField::new('update_at'),
+            TextField::new('image'),
             TextEditorField::new('content')
         ];
     }
