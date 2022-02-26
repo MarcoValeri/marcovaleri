eaee90ae516755778d02f4352ec0754d9b77f29e
Marco Valeri <marcovaleri@hotmail.it>
2022-02-26 12:44:43 +0000

    Improve article structure

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Form\Contact;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -9,14 +9,8 @@
 
 namespace App\Controller;
 
-use App\Entity\Page;
 use App\Repository\PageRepository;
-use App\Entity\Article;
-use App\Repository\ArticleRepository;
-use App\Form\Contact;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController {
@@ -30,7 +24,7 @@ class PageController extends AbstractController {
 
         return $this->render("pages/page.html.twig", [
             'pages' => $pages,
-            'slug' => $slug
+            'slug' => $slug,
         ]);
 
     }
