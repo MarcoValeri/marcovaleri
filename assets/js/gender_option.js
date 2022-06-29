3bae266e80ebdeef159694175e7c617db7320880
Marco Valeri <marcovaleri@hotmail.it>
2022-06-29 23:18:55 +0100

    Add gender banner

--- assets/js/gender_option.js
+++ assets/js/gender_option.js
@@ -1,4 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log(`Testing gender_option.js`);
+    
+    // DOM variables
+    const genderLegend = document.getElementById('gender-legend');
+    const genderMale = document.getElementById('gender-male');
+    const genderFemale = document.getElementById('gender-female');
+    const genderNonBinary = document.getElementById('gender-non-binary');
+
+    // Event Click
+    genderMale.addEventListener('click', () => {
+        genderLegend.innerHTML = 'Sentiti libero...';
+    })
+
+    genderFemale.addEventListener('click', () => {
+        genderLegend.innerHTML = "Sentiti libera...";
+    })
+
+    genderNonBinary.addEventListener('click', () => {
+        genderLegend.innerHTML = "Sentiti liberə...";
+    })
+
 })
 
