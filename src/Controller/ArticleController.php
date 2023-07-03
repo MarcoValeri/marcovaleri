21de0bc7692da921a56f5db9d7d35df1453222b7
MarcoValeri <marcovaleri@hotmail.it>
2023-07-03 14:44:42 +0100

    Add new logic on articles list

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -15,22 +15,40 @@ class ArticleController extends AbstractController {
 
         $articles = $articleRepository->findAll();
 
-        return $this->render("pages/article.html.twig", [
+        return $this->render("articles/article.html.twig", [
             'articles' => $articles,
             'slug' => $slug
         ]);
 
     }
 
-    #[Route('/articoli', name: 'app_blog')]
-    public function blog(ArticleRepository $articleRepository) {
-
-        $articles = $articleRepository->findAll();
-
-        return $this->render("pages/blog.html.twig", [
+    #[Route('/articoli', name: 'app_articles')]
+    public function blog(ArticleRepository $articleRepository, ManagerRegistry $doctrine)
+    {
+        $sqlQuery = '
+            SELECT
+                article.url,
+                article.title,
+                article.date,
+                image.file_name,
+                image.alternative_text,
+                article.content
+            FROM
+                article
+            INNER JOIN
+                image ON article.image_id = image.id
+            WHERE article.date < NOW()
+            ORDER BY date DESC
+            LIMIT 10
+        ';
+        $conn = $doctrine->getConnection();
+        $stmt = $conn->prepare($sqlQuery);
+        $result = $stmt->executeQuery();
+        $articles = $result->fetchAllAssociative();
+        
+        return $this->render("articles/articles.html.twig", [
             'articles' => $articles,
         ]);
-
     }
 
 }
