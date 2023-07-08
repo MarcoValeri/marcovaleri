d221657d1a9b4d0168dc30416d6191eb42992a16
MarcoValeri <marcovaleri@hotmail.it>
2023-07-08 13:27:38 +0100

    Update all packages

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -10,10 +10,9 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
 {
-    #[Route('categoria/{slug}/pagina_{pageNumber}', name: 'app_category')]
-    public function category(ArticleRepository $articleRepository, ManagerRegistry $doctrine, string $slug, string $pageNumber)
+    #[Route('categoria/{slugCategory}/pagina_{pageNumber}', name: 'app_category')]
+    public function category(ArticleRepository $articleRepository, ManagerRegistry $doctrine, string $slugCategory, string $pageNumber)
     {
-        // $slug = "esperienze";
 
         $fromArticleNumber = $pageNumber * 10;
         $sqlQuery = "
@@ -24,15 +23,14 @@ class CategoryController extends AbstractController
                 image.file_name,
                 image.alternative_text,
                 article.content,
-                category.name,
-                category.url
+                category.name
             FROM
                 article
             INNER JOIN
                 image ON article.image_id = image.id
             INNER JOIN
                 category ON article.category_id = category.id
-            WHERE article.date < NOW() AND category.url = '{$slug}'
+            WHERE article.date < NOW() AND category.url = '{$slugCategory}'
             ORDER BY date DESC
             LIMIT {$fromArticleNumber}, 10
         ";
@@ -46,7 +44,7 @@ class CategoryController extends AbstractController
         if (count($articles) > 0) {
             return $this->render("categories/category.html.twig", [
                 'articles'      => $articles,
-                'slug'          => $slug,
+                'slugCategory'  => $slugCategory,
                 'pageNumber'    => $pageNumber
             ]);
         } else {
