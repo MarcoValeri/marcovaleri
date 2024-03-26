11712a701617ba5b49bc877be94a188119533bd3
MarcoValeri <marcovaleri@hotmail.it>
2024-03-26 19:14:25 +0000

    Add header logic

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php
 
-// Load css
-function mv_enqueue_styles() {
-    wp_enqueue_style("main-css", get_stylesheet_directory_uri() . "./style.css");
+// Load JS and CSS files
+function mv_enqueue_script() {
+    wp_enqueue_style("style-css", get_stylesheet_directory_uri() . "./style.css");
+    wp_enqueue_style("main-css", get_stylesheet_directory_uri() . "./assets/css/main.css");
 }
-add_action("wp_enqueue_scripts", "mv_enqueue_styles");
+add_action("wp_enqueue_scripts", "mv_enqueue_script");
--- index.php
+++ index.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Marco Valeri</title>
-</head>
-<body>
+<?php get_header(); ?>
+<main>
     <h2>Marco Valeri Net</h2>
-</body>
-</html>
+</main>
+<?php get_footer(); ?>
