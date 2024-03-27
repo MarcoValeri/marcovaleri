4d962c48554dbb6de2e0552c7e1ec9440ee01421
marco.valeri <marco.valeri@cherrythinking.com>
2024-03-27 13:33:21 +0000

    Add single page template

---
+++ comments.php
@@ -0,0 +1,74 @@
+<?php
+$commentsNum = get_comments_number();
+$postID = get_post()->ID;
+?>
+<section id="article-container-comments" class="content__section-form">
+    <?php
+    // Show successfull message if the comment has been sent
+    if (count($_GET) > 0) {
+        if ($_GET['unapproved']) {
+            echo '<div class="content__container-comments-success"><p class="content__success-message body-2">Grazie per lasciato la tua opinione. Commento in fase di approvazione</p></div>';
+        }
+    }
+    ?>
+    <div class="content__form-header">
+        <h3 class="h2">Lascia un commento</h3>
+    </div>
+    <?php
+    // Set up comments form and print it out
+    $userEmailLogged = wp_get_current_user()->user_email;
+    $argsComment = [
+        'fields' => [
+            'author'    => '<div class="content__container-form-name"><input class="content__input-text input-text" type="text" id="author" name="author" placeholder="Nome *" /><div class="body-3 content__form-error">Set name error</div></div>',
+            'email'     => '<div class="content__container-form-email"><input class="content__input-text content__input-email input-text" type="email" id="email" name="email" placeholder="Email *" /><p class="body-3">La tua email non verrà pubblicata</p><div class="body-3 content__form-error">Set email error</div></div>',
+            'cookies'   => '<div class="content__container-form-privacy"><input class="input-checkbox" type="checkbox" id="Privacy" name="Privacy" required="required" value="1"><label class="content__label-privacy body-2" for="Privacy">Accetto la privacy policy</label></div><div class="article__form-comments-submit article__input"></div>'
+        ],
+        'submit_button'             => '<div class="content__container-form-submit"><input class="content__form-btn button button__black" type="submit" name="submit" value="Invia" /><p class="body-3">Per maggiori informazioni consulta la <a class="link" href="#">Privacy Policy</a></p></div>',
+        'title_reply'               => __('*** La tua email non verrà pubblicata ***'),
+        'title_reply_to'            => __('Rispondi'),
+        'comment_field'             => '<div class="content__container-form-content"><textarea id="comment" class="content__input-textarea input-textarea" name="comment" placeholder="Commento *"></textarea><div class="body-3 content__form-error">Set comment error</div></div>',
+        'logged_in_as'              => '<div><p>Sei registrato come ' . $userEmailLogged . '</p></div>',
+        'comment_notes_before'      => __(''),
+        'comment_notes_after'       => '',
+        'id_submit'                 => __('comment-submit'),
+        'class_form'                => 'content__form-grid',
+    ];
+
+    comment_form($argsComment);
+    ?>
+</section>
+<section class="content__container-comments">
+    <div class="content__comments-header">
+        <h3 class="h2"><?= $commentsNum; ?><?= $commentsNum === 1 ? " commento" : " commenti"; ?></h3>
+    </div>
+    <?php
+    // Show approved comments
+    $argsSingleComment = [
+        "status"    => "approve",
+        "post_id"   => $postID,
+    ];
+
+    $commentsQuery = new WP_Comment_Query();
+    $getAllComments = $commentsQuery->query($argsSingleComment);
+
+    if ($getAllComments) {
+        foreach ($getAllComments as $comment) {
+            ?>
+            <div class="comment-card">
+                <div class="comment-card__wrapper">
+                    <div class="comment-card__container-name">
+                        <h4 class="h3"><?= $comment->comment_author; ?></h4>
+                    </div>
+                    <div class="comment-card__container-date">
+                        <h5 class="body-4">Pubblicato il <?= get_comment_date('d-m-Y G:i:s'); ?> da <?= $comment->comment_author; ?></h5>
+                    </div>
+                    <div class="comment-card__container-comment">
+                        <p class="p"><?= $comment->comment_content; ?></p>
+                    </div>
+                </div>
+            </div>
+            <?php
+        }
+    }
+    ?>
+</section>
---
+++ single.php
@@ -0,0 +1,50 @@
+<?php get_header(); ?>
+    <?php
+    if (have_posts()) {
+        while (have_posts()) {
+            the_post();
+
+            $postID = get_the_ID();
+            $postTitle = get_the_title();
+            $postDate = get_the_date('d-m-Y');
+            $postUpdateDate = get_the_modified_date('d-m-Y');
+            $postCommentsNum = get_comments_number();
+            $postContent = get_the_content();
+        }
+    }
+    ?>
+    <article>
+        <div class="content__title">
+            <h1><?= $postTitle; ?></h1>
+        </div>
+        <div class="content__data">
+            <ul class="list-no-style">
+                <li class="content__data-item">Autore: Marco Valeri</li>
+                <li class="content__data-item">Pubblicato il: <?= $postDate; ?></li>
+                <li class="content__data-item">Ultima modifica: <?= $postUpdateDate; ?></li>
+                <li class="content__data-item">Commenti: <?= $postCommentsNum; ?></li>
+            </ul>
+        </div>
+        <div class="content__main">
+            <?= $postContent; ?>
+        </div>
+        <section>
+            <div class="banner-author">
+                <div class="banner-author__wrapper">
+                    <div class="banner-author__container-image">
+                        <img class="banner-author__image" src="#" alt="#" />
+                    </div>
+                    <div class="banner-author__container-content">
+                        <h4 class="banner-author__title h3">Marco Valeri</h4>
+                        <p class="banner-author__description p">Mi chiamo Marco Valeri, sono nato a Roma e attualmente vivo a Londra, città che mi ha cambiato la vita.<br /><br />Laureato in Computer Science alla Birkbeck University of London, divoro libri, amo scrivere e non mi stanco mai di conoscere cose nuove, soprattutto legate al mondo dell'informatica.<br /><br />Ogni giorno provo a mettere in atto il concetto secondo cui 'La disciplina è libertà'.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <?php
+        if (comments_open()) {
+            comments_template();
+        }
+        ?>
+    </article>
+<?php get_footer(); ?>
