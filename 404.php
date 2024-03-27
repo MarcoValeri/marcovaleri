525af311ef2c06b7168763ae82dedcb5bfaf416f
marco.valeri <marco.valeri@cherrythinking.com>
2024-03-27 14:31:36 +0000

    Add 404 template

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+<main class="error404">
+    <div class="error404__content-container">
+        <h1 class="error404__content h1">Pagina non trovata</h1>
+        <h2 class="error404__content h2">Errore 404</h2>
+        <h3 class="error404__content h3">www.marcovaleri.net</h3>
+        <p class="error404__content p">Sembra che la pagina che stai cercando non esiste, cosa vuoi fare?</p>
+        <div class="error404__container-btn">
+            <a href="#"><button class="error404__btn button button__black">Home</button></a>
+            <a href="#"><button class="error404__btn button button__black">Articoli</button></a>
+            <a href="#"><button class="error404__btn button button__black">Contatti</button></a>
+        </div>
+    </div>
+</main>
+<?php get_footer(); ?>
