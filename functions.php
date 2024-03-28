f37dcff954ae7dc31dab2fedcf14557e47ed0841
MarcoValeri <marcovaleri@hotmail.it>
2024-03-28 07:32:14 +0000

    Add new style path in function.php

--- functions.php
+++ functions.php
@@ -2,7 +2,23 @@
 
 // Load JS and CSS files
 function mv_enqueue_script() {
-    wp_enqueue_style("style-css", get_stylesheet_directory_uri() . "./style.css");
-    wp_enqueue_style("main-css", get_stylesheet_directory_uri() . "./assets/css/main.css");
+    wp_enqueue_style("style-css", get_template_directory_uri() . "/style.css", false, "1.1", "all");
+    wp_enqueue_style("main-css", get_template_directory_uri() . "/assets/css/main.css", false, "1.1", "all");
 }
 add_action("wp_enqueue_scripts", "mv_enqueue_script");
+
+/*
+* Create a function that get
+* @param string as path of url
+* and
+* @return bool true if
+* the current path of the url
+* has this path
+* false otherwise
+*/
+function isThisUrlPath($path) {
+    if ($_SERVER['REQUEST_URI'] === $path) {
+        return true;
+    }
+    return false;
+}
--- header.php
+++ header.php
@@ -17,27 +17,27 @@
                 <span class="navbar__span"></span>
                 <ul class="navbar__menu">
                     <li class="navbar__element navbar__logo">
-                        <a class="brand-name no-link" href="/">Marco Valeri</a>
+                        <a class="brand-name no-link" href="<?= get_site_url(); ?>">Marco Valeri</a>
                     </li>
-                    <li class="navbar__element navbar__element--mark">
+                    <li class="navbar__element <?= isThisUrlPath("/") ? 'navbar__element--mark' : '' ?>">
                         <a class="navbar__link" href="<?= get_site_url(); ?>">Home</a>
                     </li>
-                    <li class="navbar__element" >
+                    <li class="navbar__element <?= isThisUrlPath("/chi-sono") ? 'navbar__element--mark' : '' ?>" >
                         <a class="navbar__link" href="<?= get_site_url(); ?>/chi-sono">Chi sono</a>
                     </li>
-                    <li class="navbar__element">
+                    <li class="navbar__element  <?= isThisUrlPath("/category/esperienze") ? 'navbar__element--mark' : '' ?>">
                         <a class="navbar__link" href="<?= get_site_url(); ?>/category/esperienze">Esperienze</a>
                     </li>
-                    <li class="navbar__element">
+                    <li class="navbar__element <?= isThisUrlPath("/category/opportunita") ? 'navbar__element--mark' : '' ?>">
                         <a class="navbar__link" href="<?= get_site_url(); ?>/category/opportunita">Opportunità</a>
                     </li>
-                    <li class="navbar__element">
+                    <li class="navbar__element <?= isThisUrlPath("/category/vivere-estero") ? 'navbar__element--mark' : '' ?>">
                         <a class="navbar__link" href="<?= get_site_url(); ?>/category/vivere-estero">Vivere All'Estero</a>
                     </li>
-                    <li class="navbar__element">
+                    <li class="navbar__element <?= isThisUrlPath("/contatti") ? 'navbar__element--mark' : '' ?>">
                         <a class="navbar__link" href="<?= get_site_url(); ?>/contatti">Contatti</a>
                     </li>
-                    <li class="navbar__element">
+                    <li class="navbar__element <?= isThisUrlPath("/category/newsletter") ? 'navbar__element--mark' : '' ?>">
                         <a class="navbar__link" href="<?= get_site_url(); ?>/category/newsletter">Newsletter</a>
                     </li>
                 </ul>
