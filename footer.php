7ee8137c9774cd31a1e6ad43e44144272d70a48e
MarcoValeri <marcovaleri@hotmail.it>
2025-03-20 15:39:52 +0000

    Add cookie policy

--- footer.php
+++ footer.php
@@ -3,7 +3,11 @@
 <footer class="footer">
     <div class="footer__content">
         <p class="footer__content-main">Made with <span class="heart">&hearts;</span> in London by Marco Valeri - &copy; <?= date("Y"); ?> - All rights reserved</p>
-        <p class="footer__content-main"><a class="no-link--white" href="<?= get_site_url(); ?>/privacy-policy">Privacy Policy</a></p>
+        <p class="footer__content-main">
+        <a class="no-link--white" href="<?= get_site_url(); ?>/privacy-policy">Privacy Policy</a>
+        <span> - </span>
+        <a class="no-link--white" href="<?= get_site_url(); ?>/cookie-policy">Cookie Policy</a>
+        </p>
     </div>
 </footer>
 <?php wp_footer(); ?>
